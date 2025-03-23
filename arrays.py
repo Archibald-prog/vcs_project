52418cb16ff097439d62a017ca88e9ed261dfd23
[Archibald] <[archpod@gmail.com]>
2025-03-23 10:15:26 +0300

    added func_2

--- arrays.py
+++ arrays.py
@@ -9,7 +9,16 @@ def func_1(nums):
     return new_arr
 
 
+def func_2(nums):
+    new_arr = map(lambda x: nums.index(x),
+                  filter(lambda x: x % 2 == 0, nums))
+    return list(new_arr)
+
+
 if __name__ == '__main__':
     some_list = [3, 8, 4, 7, 5, 6, 2, 10]
     print(f"The exec time for func_1(): "
           f"{timeit('func_1(some_list)', globals=globals())}")
+    print('===============================================')
+    print(f"The exec time for func_2(): "
+          f"{timeit('func_2(some_list)', globals=globals())}")
